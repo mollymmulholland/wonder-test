27c6a20c6bce4b22077d174d1352665e17b94c14
mollymmulholland <mollym.mulholland@gmail.com>
2026-08-22 04:32:52 -0500

    Return element-specific interaction metadata with each assessment item

--- api/assessment/next.js
+++ api/assessment/next.js
@@ -1,4 +1,5 @@
 const {nextItem}=require('../../lib/adaptive-assessment');
+const {elementExperience}=require('../../lib/element-experience');
 
 module.exports=async function handler(req,res){
  if(req.method!=='POST')return res.status(405).json({error:'Method not allowed.'});
@@ -8,6 +9,7 @@ module.exports=async function handler(req,res){
    if(result.item){
      const {options,...rest}=result.item;
      result.item={...rest,options:options?.map(({w,dimension,...o})=>o)};
+     result.experience=elementExperience(result.item,result.element||'Ether');
    }
    return res.status(200).json(result);
  }catch(e){

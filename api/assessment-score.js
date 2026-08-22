6a0b9d4e7d33c27a136fc7ef25e357368c1cf803
mollymmulholland <mollym.mulholland@gmail.com>
2026-08-22 17:32:48 -0500

    Use expanded Wonder archetype inference

--- api/assessment-score.js
+++ api/assessment-score.js
@@ -1,4 +1,5 @@
-const {scoreResponses,inferArchetypes}=require('../lib/person-model');
+const {scoreResponses}=require('../lib/person-model');
+const {inferArchetypes,deriveFoundations,VERSION:archetype_version}=require('../lib/archetype-system-v2');
 const {buildMirror}=require('../lib/mirror-engine');
 
 module.exports=async function handler(req,res){
@@ -6,9 +7,10 @@ module.exports=async function handler(req,res){
   try{
     const responses=req.body?.responses||{};
     const model=scoreResponses(responses);
+    const foundations=deriveFoundations(model);
     const archetypes=inferArchetypes(model);
     const mirror=buildMirror(model,archetypes);
-    return res.status(200).json({model,archetypes,mirror});
+    return res.status(200).json({model:{...model,foundations},archetypes,mirror,archetype_version});
   }catch(e){
     console.error('assessment-score',e);
     return res.status(500).json({error:'Wonder could not finish the portrait yet.'});
